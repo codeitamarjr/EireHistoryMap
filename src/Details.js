50f4b46507973c025b01ba6b4a3b83ec62789124
Itamar Junior <itjunior@gmail.com>
2022-12-01 15:01:31 +0000

    Image from Unsplash

--- src/Details.js
+++ src/Details.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { View, Text, Button, Image, StyleSheet } from 'react-native';
 import EireList from './EireList';
 import { useRoute } from '@react-navigation/native';
@@ -21,13 +21,28 @@ const styles = StyleSheet.create({
 });
 
 export default class Details extends Component {
+    getImageFromUnsplash = (keyWord) => {
+        const API = 'h_hlvlx_cVGdk9zIe0qqAQ9OIeR0-gFCK1k8lpNKA3o';
+        const url = `https://api.unsplash.com/search/photos?query=${keyWord}&client_id=${API}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                if (data.results.length > 0) {
+                    urlImage = data.results[0].urls.raw;
+                }
+                // console.log(data.results[0].urls.raw);
+                this.setState({ urlImage: urlImage });
+            })
+    }
 
     constructor(props) {
         super(props);
-        console.log(props.route.params);
+        // console.log(props.route.params);
         this.state = {
             params: props.route.params,
+            urlImage: 'https://picsum.photos/300/200',
         };
+        this.getImageFromUnsplash(this.state.params.marker.name);
     }
 
     render() {
@@ -36,7 +51,7 @@ export default class Details extends Component {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text style={styles.title}>Details Screen:</Text>
-                <Image style={styles.image} source={{ uri: 'https://picsum.photos/300/200' }} />
+                <Image style={styles.image} source={{ uri: this.state.urlImage }} />
                 <Text style={styles.title}>{marker.name}</Text>
                 <Text style={styles.description}>{marker.gaelic_name}</Text>
                 <Button title="Go back" onPress={() => this.props.navigation.goBack()} />
